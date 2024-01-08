fc24068939a37661fd5782658bcf9251985c3db2
MyztiLeaf <149750780+MyztiLeaf@users.noreply.github.com>
2024-01-08 23:10:25 +0800

    (stupidly made stopped stun shelter but still trying (checking soon))

--- src/Creatures/BeePup/BupHooks.cs
+++ src/Creatures/BeePup/BupHooks.cs
@@ -7,6 +7,16 @@ public static class BupHooks
     public static void Apply()
     {
         _ = new Hook(typeof(ExtEnum<CreatureTemplate.Type>).GetMethod("op_Equality"), CursedHookDoNotCopyItWillSurelyBreakTheGame);
+        On.Player.Stun += Player_Stun;
+    }
+
+    private static void Player_Stun(On.Player.orig_Stun orig, Player self, int st)
+    {
+        if (self.isNPC && self.Template.type.value == "Bup") //todo Figuring out how to check if its in shelter or not
+        {
+            return;
+        }
+        orig(self, st);
     }
 
     private static bool CursedHookDoNotCopyItWillSurelyBreakTheGame(Func<ExtEnum<CreatureTemplate.Type>, ExtEnum<CreatureTemplate.Type>, bool> orig, ExtEnum<CreatureTemplate.Type> a, ExtEnum<CreatureTemplate.Type> b)
